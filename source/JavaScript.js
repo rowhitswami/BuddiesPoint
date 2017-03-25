975479439d8257d4ad43ca1665e7382ab3a9164d
Rowhit Swami <rowhitswami1@gmail.com>
2017-03-25 16:44:19 +0530

    first commit

---
+++ source/JavaScript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+		$("#register").click(function(){
+			$("#signupModal").modal();
+		});
+});
+$(document).ready(function(){
+		$("#login").click(function(){
+			$("#loginModal").modal();
+		});
+});
