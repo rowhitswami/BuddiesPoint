c151521519405442bcbf67df7977250f81a5f0c3
Rowhit Swami <rowhitswami1@gmail.com>
2017-04-05 02:57:27 +0530

    setting pages added

--- source/JavaScript.js
+++ source/JavaScript.js
@@ -3,8 +3,36 @@ $(document).ready(function(){
 			$("#signupModal").modal();
 		});
 });
+
 $(document).ready(function(){
 		$("#login").click(function(){
 			$("#loginModal").modal();
 		});
 });
+
+function newLocation(){
+	window.open("profile.html");
+}
+
+function demo(){
+	alert("This is a demo site having limited functionality.")
+}
+
+function validating(){
+	var username = document.getElementById("username").value;
+	var x = document.getElementById("passwor").value;
+	if (username == "rowhitswami" && x == "1234") {
+		newLocation();
+	}
+	else{
+   		alert("Please enter correct credentials...");
+	}
+	
+}
+
+function searching(){
+	var input_query = document.getElementById("search-field").onkeydown = function(event) {
+    if (event.keyCode == 13) {
+        alert('5');
+    };
+}
